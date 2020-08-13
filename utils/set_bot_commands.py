3afd0f2e51afa4a229026eff812b03177fb1710a
Дмитрий Кокорин <dmitri.kokorin@MacBook-Pro-Dmitrij.local>
2020-08-13 17:55:10 +0300

    EFLBot version 1.0

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,4 +5,8 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand("start", "Запустить бота"),
         types.BotCommand("help", "Помощь"),
+        types.BotCommand("info ", "Посмотреть информацию о текущем сезоне"),
+        types.BotCommand("standings", "Просмотреть турнирную таблицу"),
+        types.BotCommand("fixtures", "Просмотреть расписание предстоящих туров"),
+        types.BotCommand("results", "Посмотреть результаты прошедших матчей"),
     ])
